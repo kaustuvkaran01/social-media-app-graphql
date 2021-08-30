c5114d196aa4791b5d32ad24687ce30ab088947d
Kaustuv Karan <kaustuvkaran01@gmail.com>
2021-08-30 17:13:48 +0530

    add: MenuBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import {Container} from 'semantic-ui-react';
 import "semantic-ui-css/semantic.min.css";
 
 import "./App.css";
@@ -7,16 +7,18 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from './pages/Login';
 import Register from "./pages/Register";
+import MenuBar from './components/MenuBar'
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
+        <Container>
+        <MenuBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Container>
       </Router>
     </div>
   );
---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import {Link } from 'react-router-dom';
+
+export default function MenuBar() {
+  
+  const pathName = window.location.pathname;
+  const path = pathName === '/' ? 'home': pathName.substr(1);
+  
+  const [activeItem, setActiveItem] = useState(path);
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <div>
+      <Menu pointing secondary size="massive">
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/"
+        />
+        <Menu.Menu position="right">
+          <Menu.Item
+            name="register"
+            active={activeItem === "register"}
+            onClick={handleItemClick}
+            as={Link}
+            to="/register"
+          />
+          <Menu.Item
+            name="login"
+            active={activeItem === "login"}
+            onClick={handleItemClick}
+            as={Link}
+            to="/login"
+          />
+          {/* <Menu.Item
+            name="logout"
+            active={activeItem === "logout"}
+            onClick={handleItemClick}
+          /> */}
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
+}
