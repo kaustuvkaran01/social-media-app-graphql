b6ca2eda20597bd9e23e077d36117142d4ba6456
Kaustuv Karan <kaustuvkaran01@gmail.com>
2021-08-31 23:09:05 +0530

    add: Register frontend

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
 
-import { gql } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 
-export default function Register() {
+export default function Register(props) {
+  const [errors, setErrors] = useState({});
   const [values, setValues] = useState({
     username: "",
     email: "",
@@ -15,46 +16,77 @@ export default function Register() {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
+  const [addUser, { loading }] = useMutation(REGISTER_USER, {
+    update(_, result) {
+      console.log(result);
+      props.history.push("/");
+    },
+    onError(err) {
+      console.log(err.graphQLErrors[0].extensions.exception.errors);
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values,
+  });
+
   const onSubmit = (e) => {
     e.preventDefault();
     //fire a mutation to register the user.
+    addUser();
   };
-
-  //   const [addUser, {}]
   return (
-    <Form onSubmit={onSubmit}>
-      <Form.Input
-        label="Username"
-        placeholder="Username.."
-        name="username"
-        value={values.username}
-        onChange={onChange}
-      />
-      <Form.Input
-        label="Email"
-        placeholder="Email.."
-        name="email"
-        value={values.email}
-        onChange={onChange}
-      />
-      <Form.Input
-        label="Password.."
-        placeholder="Password.."
-        name="password"
-        value={values.password}
-        onChange={onChange}
-      />
-      <Form.Input
-        label="Confirm Password.."
-        placeholder="Confirm Password.."
-        name="confirmPassword"
-        value={values.confirmPassword}
-        onChange={onChange}
-      />
-      <Button type="submit" primary>
-        Register
-      </Button>
-    </Form>
+    <div className="form-container">
+      <h1>
+        <strong>Register</strong>
+      </h1>
+      <Form onSubmit={onSubmit}>
+        <Form.Input
+          label="Username"
+          placeholder="Username.."
+          name="username"
+          value={values.username}
+          onChange={onChange}
+          error={errors.username ? true : false}
+        />
+        <Form.Input
+          label="Email"
+          placeholder="Email.."
+          name="email"
+          value={values.email}
+          onChange={onChange}
+          error={errors.email ? true : false}
+        />
+        <Form.Input
+          label="Password.."
+          placeholder="Password.."
+          name="password"
+          type="password"
+          value={values.password}
+          onChange={onChange}
+          error={errors.password ? true : false}
+        />
+        <Form.Input
+          label="Confirm Password.."
+          placeholder="Confirm Password.."
+          name="confirmPassword"
+          type="password"
+          value={values.confirmPassword}
+          onChange={onChange}
+          error={errors.confirmPassword ? true : false}
+        />
+        <Button type="submit" primary>
+          Register
+        </Button>
+      </Form>
+      {Object.values(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => {
+              return <li key={value}>{value}</li>;
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
-const { ApolloServer,PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const Post = require('./models/Post');
-const User = require('./models/User');
-const { MONGODB } = require('./config');
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const Post = require("./models/Post");
+const User = require("./models/User");
+const { MONGODB } = require("./config");
 
 const pubsub = new PubSub();
 
-
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context:({req}) => ({req, pubsub})
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose.connect("mongodb+srv://kkaran3103:kkaran3103@cluster1.r8zop.mongodb.net/merng?retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology:true }).then(() => {
-    return console.log('Sever connected');
-})
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    return console.log("Sever connected");
+  });
 
-server.listen({ port: 5000 })
-.then(res => {
-    console.log(`Server running at ${res.url}`)
-})
+server.listen({ port: 5000 }).then((res) => {
+  console.log(`Server running at ${res.url}`);
+});
